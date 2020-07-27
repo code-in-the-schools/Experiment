bcc45072e8ff2ec82849aa25773f6b1c85dd2c74
Kidir Torain <kktorainnn@gmail.com>
2020-07-27 15:40:01 +0000

    rapid contract

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# function- block of code that runs after later called in the program
+#function- block of code that runs after later called in the program
 #returns a data type
+#piday= march14
+
+def my_function(m,d,y):
+ p = march 14
+m = "august"
+d = "21"
+y = "2005"
+print(m)
+print(d)
+print(y)
+
+
+
+#leo = monkey
